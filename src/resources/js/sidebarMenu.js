c1fed8429abe847e4729df5564aa203dfc42b435
BatataRaiz <joaodeamo3@gmail.com>
2023-10-02 00:56:38 -0300

    Fronted 02/10/2023

---
+++ src/resources/js/sidebarMenu.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('toggleSidebar');
+    const sidebar = document.getElementById('sidebar');
+    const headers = document.getElementById('headerContent');
+    const mainContent = document.getElementById('mainContent');
+    toggleButton.addEventListener('click', function () {
+        sidebar.classList.toggle('open'); // Adiciona ou remove a classe 'open' da Sidebar
+        mainContent.classList.toggle('expanded');
+        headers.classList.toggle('expanded');
+    });
+});
+const btnContato = document.getElementById('btnContato');
+const menuSuspensoContato = document.getElementById('menuSuspensoContato');
+
+btnContato.addEventListener('click', function () {
+    // Alterna a visibilidade do menu suspenso ao clicar no botão
+    if (menuSuspensoContato.style.display === 'none' || menuSuspensoContato.style.display === '') {
+        const contatoDev = document.createElement('p'); // Crie um novo elemento 'p'
+        contatoDev.textContent = 'Contate: joaodeamo3@gmail.com';
+
+        const br = document.createElement('br');
+        contatoDev.appendChild(br);
+
+        const linkedin = document.createElement('a');
+        linkedin.href = 'https://www.linkedin.com/in/jo%C3%A3o-victor-martins-deamo-74a3861a3/';
+        linkedin.target = '_blank';
+        linkedin.textContent = 'LinkedIn';
+
+        // Defina a propriedade white-space para preservar quebras de linha
+        contatoDev.style.whiteSpace = 'pre-line';
+
+        contatoDev.appendChild(linkedin);
+        menuSuspensoContato.appendChild(contatoDev);
+        menuSuspensoContato.style.display = 'block'; // Mostra o menu suspenso
+        menuSuspensoContato.style.color = '#fff';
+        menuSuspensoContato.style.marginBottom = '10px';
+        menuSuspensoContato.style.marginTop = '10px';
+        menuSuspensoContato.style.paddingLeft = '40px';
+        linkedin.style.lineHeight = '1.5';
+        linkedin.style.color = '#11a1df';
+    } else {
+        menuSuspensoContato.style.display = 'none'; // Esconde o menu suspenso
+    }
+});
+
+const btnAjuda = document.getElementById('btnAjuda');
+const menuSuspensoAjuda = document.getElementById('menuSuspensoAjuda');
+btnAjuda.addEventListener('click', function () {
+    // Alterna a visibilidade do menu suspenso ao clicar no botão
+    if (menuSuspensoAjuda.style.display === 'none' || menuSuspensoAjuda.style.display === '') {
+        const ajudaDev = document.createElement('p');
+        const link = document.createElement('a'); // Crie um elemento de âncora
+        link.href = 'https://www.planalto.gov.br/ccivil_03/_ato2015-2018/2018/lei/l13709.htm';
+        link.target = '_blank';
+        link.textContent = 'LGPD'; // Defina o texto âncora como "LGPD"
+
+        ajudaDev.appendChild(link); // Adicione o âncora ao parágrafo
+        menuSuspensoAjuda.appendChild(ajudaDev);
+        menuSuspensoAjuda.style.display = 'block'; // Mostra o menu suspenso
+        menuSuspensoAjuda.style.color = '#fff';
+        menuSuspensoAjuda.style.marginBottom = '10px';
+        menuSuspensoAjuda.style.marginTop = '10px';
+        menuSuspensoAjuda.style.paddingLeft = '40px';
+        link.style.color = '#fff';
+    }
+
+    else {
+        menuSuspensoAjuda.style.display = 'none'; // Esconde o menu suspenso
+    }
+}
+);
+
--- src/temp/pages/menu.php
+++ src/temp/pages/menu.php
@@ -6,7 +6,6 @@ $host = "localhost";
 $user = "root";
 $password = "";
 $database = "saepDatabase";
-
 try {
     // Conectar ao banco de dados usando PDO
     $pdo = new PDO("mysql:host=$host;dbname=$database", $user, $password);
@@ -33,58 +32,115 @@ try {
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu SAEP</title>
     <link rel="stylesheet" href="../../resources/styles/menu.css">
     <link rel="icon" href="../../resources/images/SAEP.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> <!-- Inclua o link para a biblioteca Font Awesome -->
+
+</head>
 
 <body>
-    <div class="header">
-        <div class="img-container">
-            <img src="https://images.hdqwalls.com/wallpapers/bthumb/material-style-8k-rk.jpg" alt="" />
-            <h1>Bem-vindo ao sistema SAEP, <?php if (isset($_SESSION['usuario'])) {
-                                                echo $_SESSION['usuario']['senha'];
-                                            } ?>!</h1>
-        </div>
-        <div class="sair">
-        <button id="botaoSair" class="botao-sair">Sair</button>
-        </div>
-    </div>
+    <header id="headerContent">
+        <nav>
+            <div class="navbar-container">
+                <div class="logo">
+                    <strong>Bem-vindo, <?php if (isset($_SESSION['usuario'])) {
+                                            echo $_SESSION['usuario']['senha'];
+                                        } ?>.</strong>
+                    <br>
+                    <img src="../../resources/images/SAEP.png" alt="Logo SAEP" class="logo">
+                </div>
+                <div class="menu">
+                    <div class="menuBotoes">
+                        <ul>
+                            <li><a href="#">
+                                    <button type="button" id="botaoHome" class="botao-home">Home</button>
+                                </a></li>
+                            <li><a href="https://scontent.fudi1-1.fna.fbcdn.net/v/t1.18169-9/22894102_1394105660717089_5975972865923835401_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=rN4bJWZpf8MAX_ePY7f&_nc_ht=scontent.fudi1-1.fna&oh=00_AfCGnW6pFNnhuxLfZi20J80ukaA7Sw6zeMt5D6N_5lW3aw&oe=6541A932">
+                                    <button type="button" id="botaoSobre" class="botao-sobre">Sobre</button>
+                                </a></li>
+                            <li><a href="https://res.cloudinary.com/buzzfeed-brasil/image/upload/q_auto,f_auto/image-uploads/subbuzz-images/unknown/2a288b2f34668b232687197c1d906d47.jpg">
+                                    <button type="button" id="botaoLogin" class="botao-login">Login</button>
+                                </a></li>
+                            <li><a>
+                                    <button type="button" id="botaoCadastro" class="botao-cadastro" onclick="redirecionarParaCadastros()">Cadastrar</button>
+                                </a></li>
+                            </a></li>
+                            <li><a href="mailto:joaodeamo3@gmail.com?subject=Dúvidas - SAEP">
+                                    <button type="button" id="botaoAjuda" class="botao-ajuda">Ajuda</button>
+                                </a></li>
+                            <li><a href="logout.php">
+                                    <button type="button" id="botaoSair" class="b</ul>otao-sair">Sair</button>
+                                </a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </nav>
+    </header>
+    <main id="mainContent">
+        <button type="button" title="botaoSidebar" id="toggleSidebar"><i class="fas fa-bars"></i> <!-- Use a classe de ícone do Font Awesome -->
 
-    <div class="menu">
-        <button type="button" id="botaoCadastro" onclick="redirecionarParaCadastros()">Cadastrar Atividades</button>
-    </div>
-    <div class="conteudo menu">
-        <a>Atividades</a>
-        <div class="atividades">
-            <table>
-                <tr>
-                    <th>Número da Atividade</th>
-                    <th>Nome da Atividade</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-                <?php foreach ($atividades as $atividade) : ?>
-                    <tr>
-                        <td><?php echo $atividade['numero']; ?></td>
-                        <td><?php echo $atividade['nome']; ?></td>
-                        <td><button onclick="excluirAtividade(<?php echo $atividade['numero']; ?>)">Excluir</button></td>
-                        <td><button onclick="visualizarAtividade(<?php echo $atividade['numero']; ?>)">Visualizar</button></td>
-                    </tr>
-                <?php endforeach; ?>
-            </table>
+        </button>
+
+        <!-- Sidebar -->
+        <div id="sidebar">
+            <a>
+                <button type="button" id="btnAjuda">Precisa de ajuda?</button>
+            </a>
+            <div id="menuSuspensoAjuda">
+                <p></p>
+            </div>
+
+            <button type="button" id="btnContato">Contato do Desenvolvedor</button>
+            <div id="menuSuspensoContato">
+            </div>
+
+            <a href="logout.php">
+                <button type="button" id="btnSair">Sair</button>
+            </a>
         </div>
-    </div>
+        <!--
+        <div class="saudacao">
+            <div class="img-container">
+                <img src="https://images.hdqwalls.com/wallpapers/bthumb/material-style-8k-rk.jpg" alt="" />
+            </div>
+        </div>
+                                    -->
+        <div class="conteudo">
+            <a>Atividades</a>
+            <div class="atividades">
+                <table>
+                    <tr>
+                        <th>Número da Atividade</th>
+                        <th>Nome da Atividade</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <?php foreach ($atividades as $atividade) : ?>
+                        <tr>
+                            <td><?php echo $atividade['numero']; ?></td>
+                            <td><?php echo $atividade['nome']; ?></td>
+                            <td><button type="button" onclick="excluirAtividade(<?php echo $atividade['numero']; ?>)">Excluir</button></td>
+                            <td><button type="button" onclick="visualizarAtividade(<?php echo $atividade['numero']; ?>)">Visualizar</button></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+            </div>
+        </div>
+    </main>
     <!--
     <h3>Imagem com Desfoque</h3>
     <img src="../Images/tartaruga.jpg" alt="Tartaruga" class="blur" name="tartaruga" id="tartaruga">
                 -->
-    <script src="../../resources/js/menu.js">
-    </script>
-    <div class="footer">
-        <div class="footerInfo">
-            <p>Desenvolvido por: Jvdeamo</p>
-        </div>
+    <script src="../../resources/js/menu.js"></script>
+    <script src="../../resources/js/sidebarMenu.js"></script>
+
+    <?php include('../partials/footer.html'); ?>
+
 </body>
 
 </html>
